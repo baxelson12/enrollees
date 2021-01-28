f8f1ba1f1a490f2cb0d029485a05b97a0d894f1f
Brad <bradaxelson@gmail.com>
2021-01-28 15:27:05 -0500

    feat: data service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,13 @@
 import { Component } from '@angular/core';
+import { DataService } from './core/services/data.service';
+import {
+  concatMap,
+  concatMapTo,
+  flatMap,
+  mergeMap,
+  switchMap,
+  take
+} from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +15,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'enrollees';
+  constructor(private ds: DataService) {
+    this.ds
+      .all()
+      .pipe(
+        mergeMap((d) => d),
+        concatMap((d) => this.ds.one(d.id).pipe(take(1)))
+      )
+      .subscribe(console.log);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { StoreModule } from '@ngrx/store';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { environment } from '../environments/environment';
 import { EffectsModule } from '@ngrx/effects';
+import { DataService } from './core/services/data.service';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    HttpClientModule,
     StoreModule.forRoot({}, {}),
-    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production }),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25,
+      logOnly: environment.production
+    }),
     EffectsModule.forRoot([])
   ],
-  providers: [],
+  providers: [DataService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
