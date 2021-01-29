60a876043e9b8df933de1db5f2530cb35c6e86ba
Brad <bradaxelson@gmail.com>
2021-01-29 18:45:10 -0500

    feat: enrollee card

--- src/app/core/components/enrollee/enrollee.component.ts
+++ src/app/core/components/enrollee/enrollee.component.ts
@@ -1,15 +1,58 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  Input,
+  OnDestroy,
+  ViewChild
+} from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
+import { ComponentStore } from '@ngrx/component-store';
+import { Observable, Subscription } from 'rxjs';
+import { LockableInputComponent } from '../../../shared/components/lockable-input/lockable-input.component';
+import { Enrollee } from '../../interfaces/enrollee';
+
+interface State {
+  enrollee: Enrollee;
+  locked: boolean;
+}
+
+const initial: State = {
+  enrollee: null,
+  locked: true
+};
 
 @Component({
   selector: 'app-enrollee',
   templateUrl: './enrollee.component.html',
-  styleUrls: ['./enrollee.component.scss']
+  styleUrls: ['./enrollee.component.scss'],
+  providers: [ComponentStore]
 })
-export class EnrolleeComponent implements OnInit {
+export class EnrolleeComponent implements OnDestroy {
+  @ViewChild('input') input: LockableInputComponent;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  state$: Observable<State> = this.store.select((s) => s);
+  // For cleanup
+  subscription: Subscription;
+  @Input() set enrollee(enrollee: Enrollee) {
+    this.store.patchState({ enrollee });
+  }
+  name = new FormControl('', [Validators.required]);
+  constructor(private store: ComponentStore<State>) {
+    this.store.setState(initial);
   }
 
+  unlock(): void {
+    this.store.patchState({ locked: false });
+    this.input.focus();
+  }
+
+  save(old: Enrollee): void {
+    const enrollee = { ...old, name: this.name.value };
+    this.store.patchState({ locked: true, enrollee });
+  }
+
+  // Cleanup
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
 }
--- src/app/core/components/enrollee/enrollee.module.ts
+++ src/app/core/components/enrollee/enrollee.module.ts
@@ -2,10 +2,23 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { EnrolleeComponent } from './enrollee.component';
 import { BadCardModule } from '../../../shared/components/card/card.module';
+import { AngularSvgIconModule } from 'angular-svg-icon';
+import { BadButtonModule } from '../../../shared/components/button/button.module';
+import { BadLockableInputModule } from '../../../shared/components/lockable-input/lockable-input.module';
+import { ReactiveFormsModule } from '@angular/forms';
+import { BadDirectivesModule } from '../../../shared/directives/directives.module';
 
 @NgModule({
   declarations: [EnrolleeComponent],
-  imports: [CommonModule, BadCardModule],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    BadCardModule,
+    BadButtonModule,
+    BadLockableInputModule,
+    AngularSvgIconModule,
+    BadDirectivesModule
+  ],
   exports: [EnrolleeComponent]
 })
 export class EnrolleeModule {}
