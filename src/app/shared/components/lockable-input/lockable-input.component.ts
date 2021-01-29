c1b472db94734caec00c0736adc6d0be6feb8a1e
Brad <bradaxelson@gmail.com>
2021-01-29 11:52:05 -0500

    feat: component stubs

---
+++ src/app/shared/components/lockable-input/lockable-input.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-lockable-input',
+  templateUrl: './lockable-input.component.html',
+  styleUrls: ['./lockable-input.component.scss']
+})
+export class LockableInputComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/shared/components/lockable-input/lockable-input.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LockableInputComponent } from './lockable-input.component';
+
+
+
+@NgModule({
+  declarations: [LockableInputComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class LockableInputModule { }
